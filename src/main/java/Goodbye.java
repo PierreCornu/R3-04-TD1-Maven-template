e1a5e0ac11bd41bc91197c9ac798816c5ca983ed
Pierre Cornu <161845979+PierreCornu@users.noreply.github.com>
2024-09-10 09:09:15 +0200

    Hasta la vista

--- src/main/java/Goodbye.java
+++ src/main/java/Goodbye.java
@@ -1,5 +1,6 @@
 public class Goodbye {
     public void afficherGoodbye(){
-		System.out.println("Goodbye");
+		System.out.println("Goodbye !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hasta la vista, baby !");
 	}
 }
